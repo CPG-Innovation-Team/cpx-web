6fd015e0887452f6f1c5dfbbc44521b92ef7bbb2
casualbill <hotwindzb@gmail.com>
2022-07-21 18:10:29 +0800

    Configure custom theme

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { Layout, Menu, Button, Input } from 'ant-design-vue';
 import router from './router';
 import App from './App';
 
-import 'ant-design-vue/dist/antd.css';
+import 'ant-design-vue/dist/antd.less';
 
 const app = createApp(App);
 app.use(router);
--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,22 @@ export default defineConfig({
     },
     extensions: ['.js', '.ts', '.vue'],
   },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          'primary-color': '#2A85FF',
+          'link-color': '#2A85FF',
+          'heading-color': '#1A1D1F',
+          'text-color': '#33383F',
+          'text-color-secondary': '#9A9FA5',
+          'border-radius-base': '12px',
+          'success-color': '#83BF6E',
+          'warning-color': '#FFD88D',
+          'error-color': '#FF6A55',
+        },
+      },
+    },
+  },
 });
